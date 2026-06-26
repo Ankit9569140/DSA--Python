d1874d9d89e6fabf768da116c829e33ce9443c1f
Ankit Vishwakarma <iamankit9569@gmail.com>
2026-06-26 14:24:32 +0530

    Create array_input.py

---
+++ Arrays/array_input.py
@@ -0,0 +1,21 @@
+# Array Input Example
+
+# Creating an array (list)
+arr = [10, 20, 30, 40, 50]
+
+# Printing heading
+print("Array Elements:")
+
+# Loop through the array and print each element
+for num in arr:
+    print(num)
+
+"""
+Output:
+Array Elements:
+10
+20
+30
+40
+50
+"""
